85764a33ab84a6aeb58b76eb40ac6981d6bb93f2
shaman007 <me@andreybondarenko.com>
2014-03-07 13:04:51 +0300

    Create testfrontend.java

---
+++ testfrontend.java
@@ -0,0 +1,21 @@
+/*
+ * testfrontend.java
+ * 
+ * Created on 1 Февраль 2004 г., 11:52
+ */
+/**
+ * @author shaman
+ */
+public class testfrontend {
+	/** Creates a new instance of testfrontend */
+	public testfrontend() {
+	}
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		Parse prs;
+		prs = new Parse("/Users/apple/Documents/workspace/gpss-snap/test.gpss", 0);
+	}
+}
